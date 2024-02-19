d990d8c3e6c0815188c2e896a0100a92ba307af7
raiaryan803 <raiaryan803@gmail.com>
2024-02-19 13:10:08 +0530

    folde

---
+++ PythonSeries/tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+# age2= age
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+name1 ="sk"
+name2 ='sk'
+name3='''ak'''
+print(name1)
+print(name2)
+print(name3)
+
+age=23
+old=False
+a=None
+print(type(old))
+print(type(a))
---
+++ PythonSeries/testPython.py
@@ -0,0 +1,62 @@
+print("aryan rai guru")
+print ("shradha")
+
+name = "shradha"
+age  = 23
+price = 25.66
+
+print (age)
+
+print( 25+50)
+
+print("my name is :",name)
+print ("my age is:",age)
+    
+
+# age2= age
+# print(age2)
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# name1 ="sk"
+# name2 ='sk'
+# name3='''ak'''
+# print(name1)
+# print(name2)
+# print(name3)
+
+# age=23
+# old=False
+# a=None
+# print(type(old))
+# print(type(a))
+
+a=5
+b=2
+
+print(a+b)
+print(a-b)
+print (a*b)
+print (a/b)
+print(a%b)
+
+#relational operator
+a= 50
+b= 20
+
+print (a == b) # False
+print (a != b) # True
+print (a >= b) # True
+print (a > b)  #True
+print ( a <= b) # False
+print (a < b)  # False
+
+
+ #assignment operator
+num = 20
+num += 10
+print ("num :", num)
+
+
+
+
