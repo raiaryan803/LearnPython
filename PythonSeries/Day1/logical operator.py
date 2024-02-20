ab24a5dfb1e5ee46af9ecca3438b2b4cba72466f
raiaryan803 <raiaryan803@gmail.com>
2024-02-20 13:15:48 +0530

    logical operator

---
+++ PythonSeries/Day1/logical operator.py
@@ -0,0 +1,38 @@
+# # logical operator
+# a= 50
+# b= 30
+# print (not False)
+# print (not (a>b) )
+
+# val1 = True
+# val2 = False
+# print ("and operator:", val1 and val2)
+# print ("or operator :",(a == b) or (a > b))
+
+#input
+# name = input ("enter name: ")
+# age  =int(input("enter age: "))
+# marks= float (input("entre marks: "))
+
+# print ("welcome",name)
+# print ("age =",age)
+# print ("marks=", marks)
+
+#type conversion
+# a = 2
+# b = 4.25 # 2.0 + 4.25 => 6.25
+
+# sum = a + b 
+# print (sum)
+
+# type casting
+# a = int ("2")
+# b = 4.25
+
+# print (type(a))
+# print (a + b)
+
+a= 3.45
+a =str (a)
+
+print(type(a))
