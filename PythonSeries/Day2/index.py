4973ad1021514f2420b52ab8931a5de643c0da33
raiaryan803 <raiaryan803@gmail.com>
2024-02-21 16:22:34 +0530

    slicing and indexing

---
+++ PythonSeries/Day2/index.py
@@ -0,0 +1,45 @@
+# str = "apna college"
+# ch = str [5]
+# print (ch)
+#
+
+# str = "apna college"
+# print (str [8])
+
+# slicing....
+
+str = "apna college"
+# print(str[1:5])
+# print(str[1:4])
+# print(str[:4]) #[0:4]
+# print(str[5:]) #[5:len(str)]
+# print (str[4:12])
+# print (str[4:11])
+# print (str[5:11])
+# print (str[3:11])
+# print (str[2:11])
+# print (str[1:12])
+# print (str[0:6])
+# print (str[0:8])
+print(str[3:]) #[5:len(str)]
+
+# negative indexing....
+
+str = "apple"
+print (str [-3:-1])
+print (str [-5:-1])
+print (str [-5:-2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
