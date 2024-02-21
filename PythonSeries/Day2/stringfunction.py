5ccf3d143b597e6bf89bdf59d324ac8cdde43ad8
raiaryan803 <raiaryan803@gmail.com>
2024-02-21 21:23:02 +0530

    string function

---
+++ PythonSeries/Day2/stringfunction.py
@@ -0,0 +1,21 @@
+#replace function...
+
+str = "i am studying python from apna college"
+print (str. replace("python","java script"))
+
+str = "i am studying python from apna college"
+print(str.replace("o", "a"))
+
+# find function....
+str = "i am studying python from apna college"
+print(str.find("o"))
+print(str.find("n"))
+print(str.find("p"))
+print(str.find("q"))
+
+# count function.....
+str = "i am studying python from apna college"
+print(str.count("o"))
+print(str.count("a"))
+print(str.count("from"))
+print(str.count("i"))
