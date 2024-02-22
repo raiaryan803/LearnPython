c8090db12b58ad685ed8d31627125a29b0de4643
raiaryan803 <raiaryan803@gmail.com>
2024-02-22 21:19:12 +0530

    conditional statement

---
+++ PythonSeries/Day2/conditional statement.py
@@ -0,0 +1,83 @@
+age = 18
+
+if (age>=18):
+    print("can vote & applyfor license")
+    print("can vote")
+    print("can drive")
+
+    # #2 nd type...
+    # age = 18
+    # if ("true"):
+    #      print("can vote")  
+    #      print("can drive")  
+
+    # if elif elif.....
+    # light = "green"
+    # if(light =="red"):
+    #     print("stop")
+    # elif(light=="green"):
+    #     print("go")
+    # elif(light=="yellow"):
+    #     print("look")
+
+    # print("end of code")
+
+    #if elif check.....
+
+    # num = 5
+    # if (num>2):
+    #     print ("grether than 2")
+    # elif(num>3):
+    #     print("grather than 3")
+
+
+#if if both con. check......
+    # num = 5
+    # if (num>2):
+    #       print ("grether than 2")
+    #       if(num>3):
+    #        print("grather than 3")
+
+    #        #if else statement execution....
+    #        age = 18
+    #        if (age>=18):
+    #            print ("can vote")
+    #        else:
+    #            print("cannot vote")
+
+               #if else statement ....
+
+
+    age = 15
+    if (age>=18):
+                print ("can vote")
+    else:
+                print("cannot vote")
+          
+    # if elif elif..else program.....
+    light = "pink"
+    if(light =="red"):
+        print("stop")
+    elif(light=="green"):
+        print("go")
+    elif(light=="yellow"):
+        print("look")
+    else:
+           print("light is broken") 
+
+    print("end of code")
+
+    # if elif elif..else program.....
+    light = "yellow"
+    if(light =="red"):
+        print("stop")
+    elif(light=="green"):
+        print("go")
+    elif(light=="yellow"):
+        print("look")
+    else:
+           print("light is broken") 
+
+    print("end of code")
+
+
