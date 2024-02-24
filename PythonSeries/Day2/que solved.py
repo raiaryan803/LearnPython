09cf857d15d8f8e9972df25f7804081ba78c34e7
raiaryan803 <raiaryan803@gmail.com>
2024-02-24 14:25:04 +0530

    odd even program

---
+++ PythonSeries/Day2/que solved.py
@@ -0,0 +1,35 @@
+# check the num. even or note.....
+
+# num = 124
+
+# rem = num % 2
+
+# if (rem == 0):
+#     print("even")
+# else:
+#     print ("odd")
+
+
+    # num. ko input lekar from the user .....
+
+# num = int(input("enter number: "))
+
+# rem = num % 2
+
+# if (rem == 0):
+#     print("even")
+# else:
+#     print ("odd")
+
+
+# aur bhi trah se likh sakte hai....
+
+
+num = int(input("enter number: "))
+
+
+
+if (num % 2 == 0):
+    print("even")
+else:
+    print ("odd")
