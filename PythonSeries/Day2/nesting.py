6e43a1cdd0258a20ae0aedb78e4ae2cc6c8eacaa
raiaryan803 <raiaryan803@gmail.com>
2024-02-24 12:52:16 +0530

    nasting check program

---
+++ PythonSeries/Day2/nesting.py
@@ -0,0 +1,24 @@
+# nasting condition...that means if mein if.....
+
+age = 85
+
+
+if (age >= 18):
+    if(age >= 80):
+        print("cannot drive")
+    else:
+        print("can drive")
+else:
+        print("cannot drive")
+
+#nasting condition checking.....
+
+age = 26
+
+if (age >= 18):
+    if(age >= 80):
+        print("cannot drive")
+    else:
+        print("can drive")
+else:
+        print("cannot drive")
