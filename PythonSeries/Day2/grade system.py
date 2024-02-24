d237de9687451875e3ce1893ad72cb94e7b178b5
raiaryan803 <raiaryan803@gmail.com>
2024-02-24 12:27:32 +0530

    grading system

---
+++ PythonSeries/Day2/grade system.py
@@ -0,0 +1,45 @@
+# # grand student.....
+
+marks = int(input("enter student marks : "))
+
+if (marks >= 90):
+    grade = "A"
+elif( marks >= 80 and marks < 90):
+    grade  = "B"
+elif( marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+print ("grade of the student ->" , grade)
+
+
+# grade student input lekar......
+
+marks = 95
+
+if (marks >= 90):
+    grade = "A"
+elif( marks >= 80 and marks < 90):
+    grade  = "B"
+elif( marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+print ("grade of the student ->" , grade)
+
+# grade student input lekar else ke liye check....
+
+marks =  25
+
+if (marks >= 90):
+    grade = "A"
+elif( marks >= 80 and marks < 90):
+    grade  = "B"
+elif( marks >= 70 and marks < 80):
+    grade = "C"
+else:
+    grade = "D"
+
+print ("grade of the student ->" , grade)
