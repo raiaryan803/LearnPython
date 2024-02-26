2d3dd8a4e05cabb2102bc122ae1c089fc0ee5e40
raiaryan803 <raiaryan803@gmail.com>
2024-02-26 20:45:49 +0530

    list method

---
+++ PythonSeries/Day3/list method.py
@@ -0,0 +1,49 @@
+#reverse method.....
+
+# list = ['a', 'd', 'e', 'f', 'c', 'b']
+# list.reverse()
+# print(list)
+
+# #insert method.....
+# list = [2, 6, 3]
+# list.insert(1, 5)
+# print(list)
+
+# #pop means permanantly delete....
+# list = [2, 6, 5, 3]
+# list.pop(1)
+# print(list)
+
+#append....
+list = [2 , 1, 3]
+list.append(5)
+print (list)
+
+# sorting method.....(assending order)
+list = [2, 1, 3]
+print(list.sort())
+print(list)
+
+
+# append + sorting.....
+list = [2, 1, 3]
+print(list.append(4))
+print(list.sort())
+print(list)
+
+#reverse=true....
+list = [2, 1, 3]
+print(list.append(4))
+print(list.sort(reverse=True))
+print(list)
+
+# sorting (assending order)....
+list = ["banana" , "litchi" , "apple"]
+print(list.sort())
+print(list)
+
+# reverse=true desending order......
+
+list = ["banana" , "litchi" , "apple"]
+print(list.sort(reverse=True))
+print(list)
