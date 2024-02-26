bf2720a329f38404a70c97ca0db7da24bc69bb55
raiaryan803 <raiaryan803@gmail.com>
2024-02-26 16:56:44 +0530

    day3

---
+++ PythonSeries/Day3/lecture3.py
@@ -0,0 +1,23 @@
+
+# marks = [94.4, 87.5, 95.2, 66.4, 45.1]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# #multiple data store in ....
+
+# student = ["karan" , 94.86, 16, "uttarakhand"]
+# print(student)
+
+# list slicing.......
+
+marks = [85, 94, 76, 63, 48]
+print(marks[:3])
+print(marks[:5])
+print(marks[:2])
+print(marks[1:])
+print(marks[0:])
+print(marks[3:])
+print(marks[-3:-1]) # negative indexing
