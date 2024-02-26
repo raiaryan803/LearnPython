2ad9610935fcab24995b639e9428940915314fa7
raiaryan803 <raiaryan803@gmail.com>
2024-02-26 23:15:12 +0530

    tuples

---
+++ PythonSeries/Day3/tuple.py
@@ -0,0 +1,52 @@
+# tuple....that means()....
+
+tup= (2, 1, 3, 1)
+print(type(tup))
+print(tup[0])
+print(tup[1])
+
+#2nd....
+tup = ()
+print(tup)
+print(type(tup))
+
+#3rd......
+
+tup = (1,)
+print(tup)
+print(type(tup))
+
+# 4th.....
+tup = (1)
+print(tup)
+print(type(tup))
+
+# 5th....
+tup = (1.0)
+print(tup)
+print(type(tup))
+
+# 6th...
+tup = ("hello")
+print(tup)
+print(type(tup))
+
+#  7th.....see the diffrent after , that is the understanding process.....for tuple...
+
+tup = ("hello",)
+print(tup)
+print(type(tup))
+
+# 8th....
+tup = (1, 2, 3, 4)
+print(tup)
+print(type(tup))
+
+# 9th....
+tup = (1, 2, 3, 4)
+print(tup[1:2])
+print(tup[1:3])
+
+
+
+
