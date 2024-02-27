2ca098f1509c6673999601ea1e55feaaddae5af8
raiaryan803 <raiaryan803@gmail.com>
2024-02-27 12:30:54 +0530

    tuple method

---
+++ PythonSeries/Day3/tuple methods.py
@@ -0,0 +1,12 @@
+tup = (1, 2, 3, 4, 5, 2, 2, 2)
+print(tup.count(2))
+
+#  index value....
+tup = (1, 2, 3, 4, 5, 2, 2, 2)
+print(tup.index(2))
+print(tup.index(5))
+print(tup.index(2))
+print(tup.index(1))
+
+print(tup.index(2))
+print(tup.index(5))
