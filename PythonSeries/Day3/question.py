72cfc6d7dda0b230c17bf784e0405730230dc344
raiaryan803 <raiaryan803@gmail.com>
2024-02-27 16:20:48 +0530

    koi fayda ni

---
+++ PythonSeries/Day3/question.py
@@ -0,0 +1,30 @@
+# movie program....
+
+# movies = []
+# movies.append(input("enter 1st movie: "))
+# movies.append(input("enter 2nd movie: "))
+# movies.append(input("enter 3rd movie: "))
+
+# print(movies)
+
+#  palindrome....that means opposite bhi same hi hoga ......racecar....
+
+# list1 = ["m", "a", "a", "m","p"]
+
+# copy_list1 = list1.copy()
+# copy_list1.reverse()
+
+# if(copy_list1 == list1):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# 3rd...
+grade = ("C", "D", "A", "A", "B", "B", "A" )
+print (grade.count("A"))
+
+# 4th......
+
+grade = [ "C", "D", "A", "A", "B", "B", "A"]
+grade.sort()
+print(grade)
