6d7919078e20fd1b343362c37c8f502c4e3f89be
raiaryan803 <raiaryan803@gmail.com>
2024-02-28 14:07:47 +0530

    nasted dictonary

---
+++ PythonSeries/Day4/nasteddic.py
@@ -0,0 +1,15 @@
+
+#  nasted dictonary....
+
+student = {
+    "name" : "alok rai shivam",
+    "subjects" : {
+        "phy" : 35,
+        "chem" :8,
+        "math" :10,
+    }
+}
+
+print (student)
+print(student["subjects"])
+print(student["subjects"]["chem"])
