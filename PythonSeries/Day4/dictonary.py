7334275ee391d3687c6e7823752ba2c42a9fd824
raiaryan803 <raiaryan803@gmail.com>
2024-02-28 13:53:28 +0530

    dictonary

---
+++ PythonSeries/Day4/dictonary.py
@@ -0,0 +1,22 @@
+info = {
+    "key" :"value",
+    "subjects ": ["python", "c",],
+    "topics": ("dict", "set"),
+    "name" : "apnacollege",
+    "learning" : "coding",
+    "age" : 35,
+    "is_adult" : True,
+    "marks" :  94.4
+    }
+
+info["name"] = "guru rai"
+print(info)
+
+
+null_dict = {}
+print (null_dict)
+
+
+null_dict = {}
+null_dict["name"] = "apna college"
+print (null_dict)
