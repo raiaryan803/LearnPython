309c52de7fc989c36b876f836965e15c2053dd1f
raiaryan803 <raiaryan803@gmail.com>
2024-03-07 10:58:03 +0530

    loop

---
+++ PythonSeries/Day5/loops.py
@@ -0,0 +1,107 @@
+# # ........
+# nums = [1, 2, 3, 4, 5]
+
+# for val in nums:
+#     print(val)
+# # .........
+#     veggies = ["potato", "brijal", "ladyfinger", "cucumber"]
+
+#     for val in veggies:
+#         print(val)
+# # ............
+#         tup = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+#         for num in tup:
+#             print (num)
+# ............
+# str = "apna college"
+
+# for char in str:
+#                 print (char)
+# else:
+#                 print ("end")
+
+
+# str = "apna college"
+
+# # for char in str:
+#                 if(char == 'o'):
+#                  print("o found")
+#                  break
+#                 print(char)
+# else:
+#                 print ("END")
+
+#  without else....
+                
+# str = "apna college"
+
+# for char in str:
+#                 if(char == 'o'):
+#                  print("o found")
+#                  break
+#                 print(char)
+
+# print ("END")                
+
+#  using for loop..........
+
+# nums = [ 1, 4, 9, 16, 25, 36, 49, 64, 81,100]
+
+# for el in nums:
+#     print(el)
+
+# ..using for loop...........tuple that means ()
+
+# nums =  (1, 4, 9, 16, 25, 36, 49, 64, 81,100, 49)
+
+# x =  49
+
+# idx = 0
+# for el in nums:
+#     if(el == x):
+#         print("number found at idx", idx)
+#     idx += 1
+
+# .............
+
+# nums =  (1, 4, 9, 16, 25, 36, 49, 64, 81,100, 49)
+
+# x =  49
+
+# idx = 0
+# for el in nums:
+#     if(el == x):
+#         print("number found at idx", idx)
+#         break
+#     idx += 1
+
+#range.....
+
+# seq = range (10)
+
+# for i in seq:
+#     print(i)
+
+#     # 2nd method...
+
+#     for i in range (10):
+#         print(i)
+
+
+# for i in range (2, 10):   # range(start , stop)
+#         print(i)
+
+# # ...start stop step {range}
+#         for i in range (2,  10, 2):
+#          print(i)
+
+
+#  print even num..
+        # for i in range (2,  101, 2):
+        #  print(i)
+
+
+#  print odd num (start, stop ,step)
+for i in range (1,  100, 2):
+         print(i)
