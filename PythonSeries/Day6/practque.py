c54afb13a171a3c66f3292777a9713907842445a
raiaryan803 <raiaryan803@gmail.com>
2024-03-15 12:22:51 +0530

    que factorial

---
+++ PythonSeries/Day6/practque.py
@@ -0,0 +1,18 @@
+# factorial using function......
+
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact *= i
+#     print(fact)
+    
+# cal_fact(7)
+
+
+# waf to convert usd to inr...
+
+def converter(usd_val):
+    inr_val = usd_val *83
+    print(usd_val, "usd =", inr_val, "inr")
+
+converter(15)
