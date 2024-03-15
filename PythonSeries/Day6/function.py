dc5dcaa0a5108949fe78cb837111be68731cdb25
raiaryan803 <raiaryan803@gmail.com>
2024-03-15 09:34:26 +0530

    function

---
+++ PythonSeries/Day6/function.py
@@ -0,0 +1,32 @@
+# def calc_sum(a, b):
+#     sum = a + b
+#     print(sum)
+#     return sum
+
+# calc_sum( 5, 10)
+
+# # more lines of code....
+
+# calc_sum(2,  10)
+
+# # more lines of code...
+
+# calc_sum(12, 17)
+
+# ..function defination
+
+# def calc_sum(a, b): #parameters
+#     return a + b
+
+# sum = calc_sum(178,  2221)   #function call, arguments
+# print(sum)
+
+#  average of 3 numbers....
+
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calc_avg(98, 97, 95)
